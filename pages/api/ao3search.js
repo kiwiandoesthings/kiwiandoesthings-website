d1876331063fd417e781d9f2d4a78c9413bd3bbc
Kiwian <kiwiandoesthings@gmail.com>
2026-05-18 19:38:52 -0400

    updatres people updatesd

--- pages/api/ao3search.js
+++ pages/api/ao3search.js
@@ -1,30 +1,50 @@
 var searchInput = document.getElementById("ao3-story-search");
 var searchButton = document.getElementById("ao3-search-submit");
 var searchResults = document.getElementById("story-search-results");
+var pageInput = document.getElementById("ao3-story-search-page");
 
-searchInput.onsubmit = searchStory
+searchInput.onkeydown = function(event) {
+	if (event.key === "Enter") {
+		searchStory();
+	}
+};
 searchButton.onclick = searchStory;
 
+let abortController = null;
 async function searchStory() {
-	searchResults.innerHTML = "Search results are loading...";
+	searchResults.innerHTML = "Search results are loading...<br>If it takes more than 20 seconds to load, press the search button again.";
+	if (abortController) {
+        abortController.abort();
+        console.log("Previous fetch canceled.");
+    }
 	try {
-		var searchResult = await fetch("https://api.kiwiandoesthings.place/getao3storyid?storyTitle=" + searchInput.value);
+		abortController = new AbortController();
+    	const { signal } = abortController;
+		var searchResult = await fetch("https://api.kiwiandoesthings.place/getao3storyid?storyTitle=" + searchInput.value + "&page=" + pageInput.value, {signal});
 		if (!searchResult.ok) {
             searchResults.innerHTML = "Failed to fetch search results with error \"" + searchResult.status + "\". Please retry. If the issue persists, message @KiwianDoesThings on Discord with the error message.";
             return; 
         }
 	} catch (error) {
+		if (error.name === 'AbortError') {
+            return;
+        }
+
 		searchResults.innerHTML = "Failed to fetch search results with error \"" + error + "\". Please retry. If the issue persists, message @KiwianDoesThings on Discord with the error message.";
 		return;
 	}
 	var json = await searchResult.json();
 	console.log(json);
 
+	if (json.length == 0) {
+		searchResults.innerHTML = "No search results found";
+		return;
+	}
 	searchResults.innerHTML = "";
-	Object.entries(json).forEach(([id, info]) => {
+	json.forEach((work) => {
         const div = document.createElement('li');
             
-        div.innerHTML = "<a href=\"javascript:void(0)\" onclick=\"viewStory(" + id + ", 1)\">" + info + "</a>";
+        div.innerHTML = "<a href=\"javascript:void(0)\" onclick=\"viewStory(" + work.id + ", 0)\">" + work.info + "</a>";
         searchResults.appendChild(div);
     });
 }
--- pages/api/ao3viewer.js
+++ pages/api/ao3viewer.js
@@ -2,6 +2,8 @@ var storyView = document.getElementById("ao3-story-view");
 var storyTitle = document.getElementById("ao3-story-title");
 var storyAdvance = document.getElementById("ao3-story-advance");
 var storyDevance = document.getElementById("ao3-story-devance");
+var pageInput = document.getElementById("ao3-story-goto-page");
+var pageSubmit = document.getElementById("ao3-story-goto-page-submit");
 var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
 var storyId = urlParams.get('storyID');
@@ -21,6 +23,17 @@ async function loadStory() {
 	var json = await storyResult.json();
 	storyTitle.innerHTML = json.title + ", Chapter " + json.chapter;
 	storyView.innerHTML = json.content;
+	if (json.chapterCount == json.chapter) {
+		storyAdvance.classList.add("link-disabled");
+	} else {
+		storyAdvance.classList.remove("link-disabled");
+	}
+
+	if (json.chapter == 1) {
+		storyDevance.classList.add("link-disabled");
+	} else {
+		storyDevance.classList.remove("link-disabled");
+	}
 }
 
 loadStory();
@@ -35,3 +48,7 @@ storyDevance.onclick = () => {
 	}
 	window.location = "ao3storyviewer.html?storyID=" + storyId + "&page=" + (parseInt(pageNum) - 1);
 };
+
+pageSubmit.onclick = () => {
+	window.location = "ao3storyviewer.html?storyID=" + storyId + "&page=" + (parseInt(pageSubmit.value));
+}
